39117a1f1a7f9d0544cfa7f8d1d6307cc2d88f3f
inkushwahasonu <inkushwahasonu@gmail.com>
2020-11-04 09:21:32 +0530

    solve first question of js-question in inkushwahasonu.github.io

--- projects/02-js-questions/01/script.js
+++ projects/02-js-questions/01/script.js
@@ -1,4 +1,36 @@
-let date = new Date();
-const day = document.getElementById("day");
-const time = document.getElementById("day");
-day.innerHTML=date;
+
+const dayName = document.getElementById("dayName");
+const time = document.getElementById("time");
+let today = new Date();
+let day = today.getDay();
+let dayList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+dayName.innerHTML = dayList[day] +" .";
+
+let hour = today.getHours();
+let minute = today.getMinutes();
+let second = today.getSeconds();
+let milliSecond = today.getMilliseconds();
+let prepand = (hour >= 12) ? "PM" : "AM";
+hour = (hour >= 12) ? hour - 12 : hour ;
+if (hour === 0 && prepand === ' PM '){
+  if(minute === 0 && second === 0 && milliSecond === 0){
+    hour = 12;
+    prepand = 'Noon';
+  }else{
+    hour = 12;
+    prepand = 'PM';
+  }
+}
+
+if(hour === 0 && prepand ==='AM'){
+  if(minute === 0 && second === 0 && milliSecond === 0){
+    hour = 12;
+    prepand = 'Midnight';
+  }
+  else{
+    hour = 12 ;
+    prepand = 'AM';
+  }
+}
+
+time.innerHTML = hour +" " + prepand + " : " + minute + " : " + second + " : " + milliSecond 
