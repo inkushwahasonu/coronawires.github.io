16e0cea05bd47cb65af7cedba7cf375ebf5273a7
inkushwahasonu <inkushwahasonu@gmail.com>
2020-11-05 10:08:12 +0530

    increament some function in 1st question in javascript

--- projects/02-js-questions/01/script.js
+++ projects/02-js-questions/01/script.js
@@ -1,36 +1,83 @@
+let dayList = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let monthList = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "Octuber",
+  "November",
+  "December",
+];
 
-const dayName = document.getElementById("dayName");
+const fullDate = document.getElementById("fullDate");
 const time = document.getElementById("time");
+
 let today = new Date();
+let fullyear = today.getFullYear();
+let month = today.getMonth();
+let date = today.getDate();
 let day = today.getDay();
-let dayList = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-dayName.innerHTML = dayList[day] +" .";
+
+fullDate.innerHTML =
+  fullyear + " : " + monthList[month] + " : " + date + " : " + dayList[day];
 
 let hour = today.getHours();
 let minute = today.getMinutes();
 let second = today.getSeconds();
 let milliSecond = today.getMilliseconds();
-let prepand = (hour >= 12) ? "PM" : "AM";
-hour = (hour >= 12) ? hour - 12 : hour ;
-if (hour === 0 && prepand === ' PM '){
-  if(minute === 0 && second === 0 && milliSecond === 0){
-    hour = 12;
-    prepand = 'Noon';
-  }else{
-    hour = 12;
-    prepand = 'PM';
+
+function incremetTime() {
+  second++;
+  if (second >= 60) {
+    minute++;
+    second = second - 60;
+  } else {
+    second;
   }
+
+  if (minute >= 60) {
+    hour++;
+    minute = minute - 60;
+  } else {
+    minute;
+  }
+
+  let prepand = hour >= 12 ? "PM" : "AM";
+  hour = hour >= 12 ? hour - 12 : hour;
+  if (hour === 0 && prepand === " PM ") {
+    if (minute === 0 && second === 0 && milliSecond === 0) {
+      hour = 12;
+      prepand = "Noon";
+    } else {
+      hour = 12;
+      prepand = "PM";
+    }
+  }
+
+  if (hour === 0 && prepand === "AM") {
+    if (minute === 0 && second === 0 && milliSecond === 0) {
+      hour = 12;
+      prepand = "Midnight";
+    } else {
+      hour = 12;
+      prepand = "AM";
+    }
+  }
+
+  time.innerHTML = hour + " " +prepand + " : " +minute + " : " +second + " : " +milliSecond;
 }
 
-if(hour === 0 && prepand ==='AM'){
-  if(minute === 0 && second === 0 && milliSecond === 0){
-    hour = 12;
-    prepand = 'Midnight';
-  }
-  else{
-    hour = 12 ;
-    prepand = 'AM';
-  }
-}
-
-time.innerHTML = hour +" " + prepand + " : " + minute + " : " + second + " : " + milliSecond 
+window.setInterval(incremetTime, 1000);
