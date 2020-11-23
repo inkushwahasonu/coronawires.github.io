744aac11f7805fe7bc6c9a4539907f055460dba6
inkushwahasonu <inkushwahasonu@gmail.com>
2020-11-23 18:47:02 +0530

    determine whether a given year is a leap year in the Gregorian aclender in inkushwahasonu.github.io

---
+++ projects/02-js-questions/06/script.js
@@ -0,0 +1,16 @@
+function leapyear(event) {
+  event.preventDefault();
+  const year = Number(document.getElementById("year").value);
+  const result = document.getElementById("result");
+  let leapyear = year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+
+  let message =
+    leapyear == true
+      ? (result.innerHTML = "yes! " + year + " is a leap year...")(
+          result.classList.add("true")
+        )
+      : (result.innerHTML = "No! " + year + " is not a leap year...")(
+          result.classList.add("false")
+        );
+  result.innerHTML = message;
+}
