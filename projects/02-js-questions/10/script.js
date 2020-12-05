047e970daf00b6e801ee08852a14c63edc468e45
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-05 19:12:42 +0530

    incomplete

--- projects/02-js-questions/10/script.js
+++ projects/02-js-questions/10/script.js
@@ -1,18 +1,15 @@
-const num1 = document.getElementById('num1').value;
-const num2 = document.getElementById('num2').value;
-const mul = document.getElementById('mul');
-const div = document.getElementById('div');
-const reset = document.getElementById('reset');
-const result = document.getElementById('result');
+const num1 = document.getElementById("num1").value;
+const num2 = document.getElementById("num2").value;
+const result = document.getElementById("result");
 
-function multi(event){
-  event.preventDefault();
-  let mul = num1 + num2;
+function multi() {
+ 
+  let mul = num1 * num2;
   result.innerHTML = mul;
 }
-function division(event2){
+function division(event2) {
   event2.preventDefault();
 }
-function reset(event3){
+function reset(event3) {
   event3.preventDefault();
 }
