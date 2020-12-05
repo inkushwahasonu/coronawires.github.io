d80ab9e057353cd569722c5a32a625d5b62f3554
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-05 16:36:17 +0530

    edit

--- projects/02-js-questions/09/script.js
+++ projects/02-js-questions/09/script.js
@@ -1,16 +1,15 @@
-const newChristmas = document.getElementById('newChristmas');
-const message = document.getElementById('message');
+const newChristmas = document.getElementById("newChristmas");
+const message = document.getElementById("message");
 
 let d = new Date();
 
-
 const cmas = new Date(d.getFullYear(), 11, 25);
 newChristmas.innerHTML = cmas;
-if (d.getMonth()==11 && d.getDay()>25){
-  cmas.setFullYear(cmas.getFullYear+1);
+if (d.getMonth() == 11 && d.getDay() > 25) {
+  cmas.setFullYear(cmas.getFullYear + 1);
 }
-const one_day = 1000*60*60*24;
+const one_day = 1000 * 60 * 60 * 24;
 
-
-
-message.innerHTML = Math.ceil((cmas.getTime()-d.getTime())/one_day) + " day's left until christmas!";
+message.innerHTML =
+  Math.ceil((cmas.getTime() - d.getTime()) / one_day) +
+  " day's left until christmas!";
