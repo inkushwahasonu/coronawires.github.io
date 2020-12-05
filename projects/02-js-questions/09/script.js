087c49c374ef9f412c71fc63597b627c6b576c8a
inkushwahasonu <inkushwahasonu@gmail.com>
2020-12-05 10:07:47 +0530

    remaining day until christmas in inkushwahasonu.github.io

--- projects/02-js-questions/09/script.js
+++ projects/02-js-questions/09/script.js
@@ -1,7 +1,16 @@
 const newChristmas = document.getElementById('newChristmas');
+const message = document.getElementById('message');
 
 let d = new Date();
-let year = d.getFullYear();
-let month = d.getMonth();
-let date = d.getDate();
-newChristmas.innerHTML = date + " : " + month + " : " + year;
+
+
+const cmas = new Date(d.getFullYear(), 11, 25);
+newChristmas.innerHTML = cmas;
+if (d.getMonth()==11 && d.getDay()>25){
+  cmas.setFullYear(cmas.getFullYear+1);
+}
+const one_day = 1000*60*60*24;
+
+
+
+message.innerHTML = Math.ceil((cmas.getTime()-d.getTime())/one_day) + " day's left until christmas!";
